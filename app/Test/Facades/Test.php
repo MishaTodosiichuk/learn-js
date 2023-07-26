7cd9ff9661203d5c9489470c04393046b0a1bf66
Михайло <todosiichukm@gmail.com>
2023-07-26 20:43:05 +0300

    6 How it works

--- app/Test/Facades/Test.php
+++ app/Test/Facades/Test.php
@@ -1,1 +1,12 @@
 <?php
+
+namespace App\Test\Facades;
+use Illuminate\Support\Facades\Facade;
+
+class Test extends Facade
+{
+    public static function getFacadeAccessor()
+    {
+        return 'test';
+    }
+}
