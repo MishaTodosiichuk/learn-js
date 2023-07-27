249f6c4c8fb39201394722d5e2bd3f0b04b63ad2
Михайло <todosiichukm@gmail.com>
2023-07-27 16:47:34 +0300

    Create route for PostController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +17,21 @@ Route::get('test', TestController::class);
 
 //CRUD (create, read, update, delete)
 
+Route::get('posts', [PostController::class, 'index'])->name('posts');
 
+Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
+
+Route::post('posts/create', [PostController::class, 'store'])->name('posts.store');
+
+Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
+Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+
+Route::put('posts/{post}', [PostController::class, 'update'])->name('posts.update');
+
+Route::delete('posts/{post}/destroy', [PostController::class, 'destroy'])->name('posts.destroy');
+
+Route::put('posts/{post}', [PostController::class, 'like'])->name('posts.like');
 
 
 
